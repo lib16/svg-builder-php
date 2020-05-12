7486195747ef166e015453b61dcc49e3db0a735e
M. Hoffmann <git@applications-cologne.com>
2020-05-12 11:18:26 +0200

    phpunit bootstrap for Travis CI (1)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,7 @@
 <?php
 require_once 'vendor/autoload.php';
+require_once 'src/Svg.php';
+require_once 'src/enums/PreserveAspectRatio.php';
+require_once 'src/enums/SpreadMethod.php';
+require_once 'src/enums/Units.php';
+require_once 'vendor/lib16/xml/src/shared/ClassAttribute.php';
