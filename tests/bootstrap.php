85caa2b0f3cd9eb5d383270a447b3ef188450c0e
M. Hoffmann <git@applications-cologne.com>
2020-05-12 11:49:51 +0200

    phpunit bootstrap for Travis CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,19 @@
 require_once 'vendor/autoload.php';
 
 // @todo for Travis CI
+require_once 'vendor/lib16/utils/src/NumberFormatter.php';
 require_once 'vendor/lib16/utils/src/enums/css/Unit.php';
+require_once 'vendor/lib16/utils/src/enums/css/AngleUnit.php';
+require_once 'vendor/lib16/utils/src/enums/css/FrequencyUnit.php';
 require_once 'vendor/lib16/utils/src/enums/css/LengthUnit.php';
+require_once 'vendor/lib16/utils/src/enums/css/TimeUnit.php';
 require_once 'vendor/lib16/utils/src/enums/css/Media.php';
 require_once 'vendor/lib16/utils/src/enums/mime/MimeType.php';
+require_once 'vendor/lib16/utils/src/enums/mime/AudioType.php';
+require_once 'vendor/lib16/utils/src/enums/mime/IconType.php';
+require_once 'vendor/lib16/utils/src/enums/mime/ImageType.php';
 require_once 'vendor/lib16/utils/src/enums/mime/StyleType.php';
+require_once 'vendor/lib16/utils/src/enums/mime/VideoType.php';
 require_once 'vendor/lib16/xml/src/Adhoc.php';
 require_once 'vendor/lib16/xml/src/Attributes.php';
 require_once 'vendor/lib16/xml/src/ProcessingInstruction.php';
